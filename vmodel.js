899ed3d06bf13728e0bf448f12f9ab4cb856191b
Kamezaki-jp <champagne.es335@gmail.com>
2021-06-18 15:31:08 +0900

    watch practice

--- vmodel.js
+++ vmodel.js
@@ -7,5 +7,13 @@ new Vue ({
     lessThanThree: function() {
       return this.counter > 3 ? '3より上' : '3以下'
     }
+  },
+  watch: {
+    counter: function() {
+      var vm = this;
+      setTimeout(function(){
+        vm.counter =  0;
+      }, 3000)
+    }
   }
 })
