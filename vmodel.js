02b6f40d41e0a3cadc5b0790beeeffdad7362cc4
Kamezaki-jp <champagne.es335@gmail.com>
2021-06-18 12:53:38 +0900

    computed practice

--- vmodel.js
+++ vmodel.js
@@ -1,7 +1,11 @@
 new Vue ({
   el: '#app',
-  // モデル
   data: {
-    message: 'こんにちは'
+    counter: 0,
+  },
+  computed: {
+    lessThanThree: function() {
+      return this.counter > 3 ? '3より上' : '3以下'
+    }
   }
 })
