0a7f146c91d5ac0243950c7ff4a9989fd10f41ed
Kamezaki-jp <champagne.es335@gmail.com>
2021-06-24 17:14:28 +0900

    vue instance practice

--- section4/vue_instance.js
+++ section4/vue_instance.js
@@ -1,15 +1,23 @@
 // できるだけ複数インスタンスは作らないほうが良い
 // app1 app2のように独立しているときは使ってもいい
-new Vue({
+var vm1 = new Vue({
   el: '#app1',
   data: {
     message: 'インスタンス1'
   }
 })
 
-new Vue({
+// インスタンスの外側から操作する
+vm1.message = '書き換えられました。'
+
+var vm2 = new Vue({
   el: '#app2',
   data: {
     message: 'インスタンス2'
+  },
+  methods: {
+    changeMessage1: function() {
+      vm1.message = 'インスタンス2から変更しました。'
+    }
   }
 })
