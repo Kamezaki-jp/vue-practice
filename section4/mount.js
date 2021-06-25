b368ce7fec62ef4e330e3b78c01abe118a5d1775
Kamezaki-jp <champagne.es335@gmail.com>
2021-06-25 14:32:36 +0900

    mount practice

---
+++ section4/mount.js
@@ -0,0 +1,37 @@
+var data = {
+  message: 'こんにちは',
+  name: 'よしぴー'
+}
+
+var vm = new Vue({
+  // el: '#app',
+  data: data,
+  computed: {
+    myData: function() {
+      return this.$data;
+    }
+  }
+})
+
+// el: プロパティの代わりになる
+vm.$mount('#app')
+
+new Vue({
+  // el: '#app2',
+  data: {
+    name: 'よしぴー'
+  },
+  template: '<h1>こんにちは、{{name}}</h1>'
+}).$mount('#app2')
+
+new Vue({
+  data: {
+    name: 'よしぴー'
+  },
+  render: function(createElement) {
+    return createElement('h1', 'こんにちは、' + this.name);
+  },
+}).$mount('#app3')
+
+var dir = document.createElement('div');
+console.log(dir);
