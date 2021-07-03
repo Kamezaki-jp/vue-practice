9e5693bf7ea15e749d80621734e4ca4e7ef2b239
Kamezaki-jp <champagne.es335@gmail.com>
2021-07-03 11:08:53 +0900

    global local component practice

--- section6/component.js
+++ section6/component.js
@@ -1,8 +1,6 @@
-// コンポーネント
-// Vue.component('ファイル名', {オブジェクト})
-Vue.component('my-component', {
-  // コンポーネントではdataを使う時に関数にする必要がある
-  data: function() { 
+// コンポーネント（ローカル登録）
+var component = {
+  data: function() {
     return {
       number: 12
     }
@@ -13,8 +11,34 @@ Vue.component('my-component', {
       this.number += 1;
     }
   }
-})
+}
+
+// コンポーネント(グローバル登録)
+// Vue.component('ファイル名', {オブジェクト})
+// Vue.component('my-component', {
+  // コンポーネントではdataを使う時に関数にする必要がある
+//   data: function() { 
+//     return {
+//       number: 12
+//     }
+//   },
+//   template: '<p>いいね({{number}})<button @click="increment">+1</button></p>',
+//   methods: {
+//     increment: function() {
+//       this.number += 1;
+//     }
+//   }
+// })
 
 new Vue({
 	el: '#app',
+  // ローカル登録
+  components: {
+    // 'ファイル名' : 値（変数名）
+    'my-component': component
+  }
+})
+
+new Vue({
+	el: '#app2',
 })
