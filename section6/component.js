2aea039d4d8e8f3b50f613842e25fd24532fdaea
Kamezaki-jp <champagne.es335@gmail.com>
2021-07-03 10:42:02 +0900

    component practice

--- section6/component.js
+++ section6/component.js
@@ -1,7 +1,15 @@
-new Vue({
-	el: '#app',
-  data: {
-  	number: 12
+// コンポーネント
+// Vue.component('ファイル名', {オブジェクト})
+Vue.component('my-component', {
+  // コンポーネントではdataを使う時に関数にする必要がある
+  data: function() { 
+    return {
+      number: 12
+    }
   },
   template: '<p>いいね({{number}})</p>'
+})
+
+new Vue({
+	el: '#app',
 })
