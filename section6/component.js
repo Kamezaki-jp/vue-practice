da941c23d7fb52f6d15ba0b45e7e7276fb1e58c8
Kamezaki-jp <champagne.es335@gmail.com>
2021-07-03 10:49:56 +0900

    component practice

--- section6/component.js
+++ section6/component.js
@@ -7,7 +7,12 @@ Vue.component('my-component', {
       number: 12
     }
   },
-  template: '<p>いいね({{number}})</p>'
+  template: '<p>いいね({{number}})<button @click="increment">+1</button></p>',
+  methods: {
+    increment: function() {
+      this.number += 1;
+    }
+  }
 })
 
 new Vue({
